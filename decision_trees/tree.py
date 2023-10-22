2878bff00052eb3f1ac9c6e2ddcf00d09f12a76d
Grzegorz Płaczek <g01.placzek@gmail.com>
2023-10-22 19:00:01 +0200

    Tree refactor

--- decision_trees/tree.py
+++ decision_trees/tree.py
@@ -1,7 +1,9 @@
 from .metrics import entropy, gain_ratio
+
+import json
+
 import pandas as pd
 import random
-import json
 import graphviz
 
 edges=[]
@@ -14,31 +16,36 @@ def make_tree(
     features: list[str],
     linear_featers: list[str] = [],
     idx: str = '0',
-    last_best_feature_value: str = '-1',
+    last_best_feature: str = '-1',
     value: str = '0'
 ) -> dict:
-    if len(features) == 0 or len(set(data[target])) == 1:
+    if len(features) == 0 or len(data[target].unique()) == 1:
         d=data[target].value_counts().nlargest(1).index[0]
-        dot.node(last_best_feature + str(d), str(d))
-        dot.edge(last_best_feature, last_best_feature +str(d),label=value)
+
+        dot.node(f'{last_best_feature}{d}', str(d))
+        dot.edge(last_best_feature, f'{last_best_feature}{d}', label=value)
+
         return d
 
     best_feature = max(features, key=lambda f: gain_ratio(data, f, target))
+
     # connections betweens subtrees
     if last_best_feature != '-1':
-        dot.node(str(best_feature)+idx,best_feature)
-        dot.edge(last_best_feature,str(best_feature) + idx,label=value)
+        dot.node(f'{best_feature}{idx}', best_feature)
+        dot.edge(last_best_feature, f'{best_feature}{idx}', label=value)
     else:
-        dot.node(str(best_feature)+idx,best_feature)
-    tree = {best_feature: {}}
+        dot.node(f'{best_feature}{idx}',best_feature)
+
+    tree = { best_feature: {} }
     features = [f for f in features if f != best_feature]
 
     idm = len(data[best_feature].unique())
 
     if best_feature not in linear_featers:
-        for idxs,value in enumerate(data[best_feature].unique()):
+        for idxs, value in enumerate(data[best_feature].unique()):
             subset = data[data[best_feature] == value]
-            tree[best_feature][str(value)] = make_tree(subset, target, features,linear_featers,idx+str(idxs),str(best_feature)+ idx,str(value))
+            tree[best_feature][str(value)] = make_tree(subset, target, features, linear_featers, f'{idx}{idxs}', f'{best_feature}{idx}', str(value))
+
     # else make divisions
 
     return tree
