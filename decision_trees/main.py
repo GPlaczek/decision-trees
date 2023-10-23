8ca985c3bf0a16757cd34a7de5a9a0958c02f6e2
Unun0kwaD <pawel.marczewski@student.put.poznan.pl>
2023-10-23 23:36:35 +0200

    tree edges albeling and age labels added

--- decision_trees/main.py
+++ decision_trees/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from .tree import make_tree, print_tree
+from .tree import make_tree, print_tree, map_age_to_labels
 import pandas as pd
 
 
@@ -8,9 +8,11 @@ def main():
     print("Hello world")
     data = pd.read_csv('res/titanic-homework.csv')
     target = 'Survived'
-    features = ['Pclass', 'Sex', 'SibSp', 'Parch']
+    features = ['Pclass', 'Sex', 'SibSp', 'Parch','Age']
+    data['Age']=data['Age'].apply(map_age_to_labels)
     linear_featers=['Age']
-    print_tree(make_tree(data,target,features,linear_featers))
+
+    print_tree(make_tree(data,target,features))
     
 
 if __name__ == "__main__":
--- decision_trees/tree.py
+++ decision_trees/tree.py
@@ -7,24 +7,23 @@ import graphviz
 edges=[]
 dot = graphviz.Digraph()
 
-def make_tree(data,target,features,linear_featers,idx='0',last_best_feature_value='-1'):
+def make_tree(data,target,features,linear_featers=[],idx='0',last_best_feature='-1',value='0'):
 
     # print(last_best_feature_value)
     # print(set(data[target]))
     # print(len(set(data[target])))
     if len(features) == 0 or len(set(data[target])) == 1:
         d=data[target].value_counts().nlargest(1).index[0]
-        dot.node(last_best_feature_value + str(d), str(d))
-        dot.edge(last_best_feature_value, last_best_feature_value +str(d))
+        dot.node(last_best_feature + str(d), str(d))
+        dot.edge(last_best_feature, last_best_feature +str(d),label=value)
         return d
         
         
-
     best_feature = max(features, key=lambda f: gain_ratio(data, f, target))
     # connections betweens subtrees
-    if last_best_feature_value != '-1':
+    if last_best_feature != '-1':
         dot.node(str(best_feature)+idx,best_feature)
-        dot.edge(last_best_feature_value,str(best_feature) + idx)
+        dot.edge(last_best_feature,str(best_feature) + idx,label=value)
     else:
         dot.node(str(best_feature)+idx,best_feature)
     tree = {best_feature: {}}
@@ -34,15 +33,22 @@ def make_tree(data,target,features,linear_featers,idx='0',last_best_feature_valu
 
     if best_feature not in linear_featers:
         for idxs,value in enumerate(data[best_feature].unique()):
-            dot.node(str(str(value)+'_' + idx+str(idxs)),str(value))
-            dot.edge(str(best_feature) + idx,str(value)+'_' + idx+str(idxs))
             subset = data[data[best_feature] == value]
-            tree[best_feature][str(value)] = make_tree(subset, target, features,linear_featers,idx+str(idxs),str(value)+'_' + idx+str(idxs))
+            tree[best_feature][str(value)] = make_tree(subset, target, features,linear_featers,idx+str(idxs),str(best_feature)+ idx,str(value))
     # else make divisions
 
     return tree
     
 
+def map_age_to_labels(age):
+    if 0 <= age <= 20:
+        return "young"
+    elif 20 < age <= 40:
+        return "middle"
+    elif 40 < age <= 100:
+        return "old"
+    else:
+        return "unknown"
 
 def print_tree(tree):
     # print( json.dumps(tree)
